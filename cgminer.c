0cbb58606986aa9c276db467162629274fdc6034
csa402 <csa402@gmail.com>
2018-09-29 18:13:52 +0200

    update

--- cgminer.c
+++ cgminer.c
@@ -6911,7 +6911,7 @@ static void *stratum_sthread(void *userdata)
 		if (unlikely(pool->removed))
 			break;
 
-		work = tq_pop(pool->stratum_q);
+		work = tq_pop(pool->stratum_q, NULL);
 		if (unlikely(!work))
 			quit(1, "Stratum q returned empty work");
 
